a7537c7d6eb664748c50b4d5fbd45f9d47ad6ad7
Mihir Gokani <mihirgokani007@yahoo.co.in>
2022-03-31 22:50:51 +0530

    Move idleState globally and allow run for duration

--- src/Blinker.cpp
+++ src/Blinker.cpp
@@ -1,12 +1,13 @@
 #include <Blinker.h>
 
-Blinker::Blinker(int pin) {
+Blinker::Blinker(int pin, bool idleState) {
     _blinkPin = pin;
     _highDelay = 500;
     _lowDelay = 500;
     _lastBlink = 0;
     _blinkState = false;
     _running = false;
+    _idleState = idleState;
 }
 
 void Blinker::setDelay(uint32_t d) {
@@ -20,25 +21,28 @@ void Blinker::setDelay(uint32_t h, uint32_t l) {
 }
 
 void Blinker::blink() {
-    uint32_t diff = millis() - _lastBlink;
-    uint32_t blinkDelay = _blinkState ? _highDelay : _lowDelay;
-    if (diff >= blinkDelay) {
-        _lastBlink = millis() - (diff - blinkDelay); // Account for any delay in calling blink
-        _blinkState = !_blinkState;
-        if (_running) {
+    if (running) {
+        uint32_t diff = millis() - _lastBlink;
+        uint32_t blinkDelay = _blinkState ? _highDelay : _lowDelay;
+        _duration -= diff;
+        if (_duration <= 0) {
+            stop();
+        } else if (diff >= blinkDelay) {
+            _lastBlink = millis() - (diff - blinkDelay); // Account for any delay in calling blink
+            _blinkState = !_blinkState;
             digitalWrite(_blinkPin, _blinkState ? HIGH : LOW);
         }
     }
 }
 
-void Blinker::start() {
+void Blinker::start(double duration) {
     pinMode(_blinkPin, OUTPUT);
     _running = true;
+    _duration = duration;
     _lastBlink = millis();
 }
 
-void Blinker::stop(bool idleState) {
+void Blinker::stop() {
     _running = false;
-    _blinkState = idleState;
-    digitalWrite(_blinkPin, _blinkState);
+    digitalWrite(_blinkPin, _idleState);
 }
